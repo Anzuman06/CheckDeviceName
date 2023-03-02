88cf2677dcf42e865ccedf0c55738eeb76d22a82
Snigdha Anzuman <snigdha@meldcx.com>
2023-03-02 15:46:42 +0600

    first commit

---
+++ cypress/e2e/1st.cy.js
@@ -0,0 +1,13 @@
+it('Log in to Canary', function(){
+  cy.visit('https://canary.api.meld.cx/login'),
+  cy.get(':nth-child(2) > span > input').type(Cypress.env('email')),
+  cy.get(':nth-child(3) > span > input').type(Cypress.env('password')),
+  cy.get('.glow > span').click(),
+  //cy.get('.nav-bar-burger').click(),
+  cy.get('.devices-recent > .content'),
+  cy.get('[href="/device/3929"] > span.ellipsis').click(),
+  cy.get(':nth-child(2) > :nth-child(2) > .label'),
+  cy.get(':nth-child(2) > :nth-child(2) > input').should('have.value', Cypress.env('chromedevicename'))
+
+})
+
